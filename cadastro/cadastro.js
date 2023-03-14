97cea2704b7bc7763a3cfdf041dd9202d0a1d545
Mateus V. Gomes <monkmitogames@hotmail.com>
2023-03-14 19:22:15 +0000

    Implementado função de cadastro

---
+++ cadastro/cadastro.js
@@ -0,0 +1,21 @@
+const specialCharacterRegex = /[\W]/g;
+const upperCaseCharacterRegex = /[A-Z]/g;
+const formInput = document.getElementById("formRegister");
+const inputPassword = document.getElementById("inputPassword");
+inputPassword.addEventListener("change", function () {
+  if (specialCharacterRegex.test(inputPassword.value) == true) {
+    console.log('Caracter especial presente');
+  }
+else{
+    console.log('Caracter especial não presente');
+}
+if(upperCaseCharacterRegex.test(inputPassword.value) == true){
+    console.log('Letra maiscula presente');
+}
+else{
+    console.log('letra maiscula não presente');
+}
+
+});
+
+function verifyPassword() {}
